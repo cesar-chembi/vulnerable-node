5d2cc382e3e6f2f540940dc8e49424942dd0a4e3
cesar44@gmail.com <cesar44@gmail.com>
2022-11-02 10:08:30 -0500

    Modificacion fuente  web

--- model/auth.js
+++ model/auth.js
@@ -6,6 +6,16 @@ function do_auth(username, password) {
 
     var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
 
+
+/*
+    var name = req.query.name; // user-controlled input
+    var password = crypto.createHash('sha256').update(req.query.password).digest('base64');
+
+    var sql = "select * from user where name = ? and password = ?"; // the query is parameterized
+*/
+
+
+
     return db.one(q);
 }
 
